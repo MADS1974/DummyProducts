5eb5d1fd67650faf6d7c160ada1032dcf335468f
MADS <deejaykbello@hotmail.com>
2026-02-28 17:10:58 -0300

    Iniciando estrutura da MainActivity e Adapter

--- app/src/main/java/br/edu/ifsp/scl/sdm/dummyproducts/ui/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/sdm/dummyproducts/ui/MainActivity.kt
@@ -3,18 +3,28 @@ package br.edu.ifsp.scl.sdm.dummyproducts.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import br.edu.ifsp.scl.sdm.dummyproducts.R
+import br.edu.ifsp.scl.sdm.dummyproducts.adapter.ProductAdapter
 import br.edu.ifsp.scl.sdm.dummyproducts.databinding.ActivityMainBinding
+import br.edu.ifsp.scl.sdm.dummyproducts.model.Product
 
 class MainActivity : AppCompatActivity() {
     private val amb: ActivityMainBinding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
+    private val productList: MutableList<Product> = mutableListOf()
+    private val productAdapter: ProductAdapter by lazy {
+        ProductAdapter(this, productList)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(amb.root)
 
         setSupportActionBar(amb.mainTb.apply {
-            title = getString(R.string.app_name)
+            title = "DummyProducts"
         })
+
+        amb.productsSp.adapter = productAdapter
+
     }
 }
