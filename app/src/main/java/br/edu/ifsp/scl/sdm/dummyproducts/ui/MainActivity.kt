7d6317dce7406bc2ac038fd33db90afc307d9ff7
MADS <deejaykbello@hotmail.com>
2026-03-02 14:13:20 -0300

    Implementação do carregamento de imagens de produtos via URL com Threading e RecyclerView

--- app/src/main/java/br/edu/ifsp/scl/sdm/dummyproducts/ui/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/sdm/dummyproducts/ui/MainActivity.kt
@@ -1,10 +1,15 @@
 package br.edu.ifsp.scl.sdm.dummyproducts.ui
 
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
 import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
 import br.edu.ifsp.scl.sdm.dummyproducts.R
 import br.edu.ifsp.scl.sdm.dummyproducts.adapter.ProductAdapter
+import br.edu.ifsp.scl.sdm.dummyproducts.adapter.ProductImageAdapter
 import br.edu.ifsp.scl.sdm.dummyproducts.databinding.ActivityMainBinding
 import br.edu.ifsp.scl.sdm.dummyproducts.model.Product
 import br.edu.ifsp.scl.sdm.dummyproducts.model.ProductList
@@ -15,6 +20,9 @@ import java.io.InputStreamReader
 import java.net.HttpURLConnection
 import java.net.HttpURLConnection.HTTP_OK
 import java.net.URL
+import kotlin.collections.forEach
+import java.io.BufferedInputStream
+import android.graphics.BitmapFactory
 
 
 class MainActivity : AppCompatActivity() {
@@ -25,6 +33,12 @@ class MainActivity : AppCompatActivity() {
     private val productAdapter: ProductAdapter by lazy {
         ProductAdapter(this, productList)
     }
+    private val productImageList: MutableList<Bitmap> = mutableListOf()
+    private val productImageAdapter: ProductImageAdapter by lazy {
+        ProductImageAdapter(this, productImageList)
+    }
+
+
 
     companion object {
         const val PRODUCT_ENDPOINT = "https://dummyjson.com/products/"
@@ -38,7 +52,32 @@ class MainActivity : AppCompatActivity() {
             title = "DummyProducts"
         })
 
-        amb.productsSp.adapter = productAdapter
+        amb.productsSp.apply {
+            adapter = productAdapter
+            onItemSelectedListener = object: AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(
+                    parent: AdapterView<*>?,
+                    view: View?,
+                    position: Int,
+                    id: Long
+                ) {
+                    val size = productImageList.size
+                    productImageList.clear()
+                    productImageAdapter.notifyItemRangeRemoved(0, size)
+                    retrieveProductImages(productList[position])
+                }
+
+                override fun onNothingSelected(parent: AdapterView<*>?) {
+                    // NSA
+                }
+            }
+        }
+
+        amb.productImagesRv.apply {
+            layoutManager = LinearLayoutManager(this@MainActivity)
+            adapter = productImageAdapter
+        }
+
         retrieveProducts()
     }
 
@@ -69,6 +108,30 @@ class MainActivity : AppCompatActivity() {
             }
         } finally {
             productsConnection.disconnect()
+        }
+    }.start()
+
+    private fun retrieveProductImages(product: Product) = Thread {
+        product.images.forEach { imageUrl ->
+            val imageConnection = URL(imageUrl).openConnection() as HttpURLConnection
+            try {
+                if (imageConnection.responseCode == HTTP_OK) {
+                    BufferedInputStream(imageConnection.inputStream).let {
+                        val imageBitmap = BitmapFactory.decodeStream(it)
+                        runOnUiThread {
+                            productImageList.add(imageBitmap)
+                            productImageAdapter.notifyItemInserted(productImageList.lastIndex)
+                        }
+
+                    }
+                } else {
+                    runOnUiThread { Toast.makeText(this, getString(R.string.request_problem), Toast.LENGTH_SHORT).show() }
+                }
+            } catch (ioe: IOException) {
+                runOnUiThread { Toast.makeText(this, getString(R.string.connection_failed), Toast.LENGTH_SHORT).show() }
+            } finally {
+                imageConnection.disconnect()
+            }
         }
     }.start()
 }
